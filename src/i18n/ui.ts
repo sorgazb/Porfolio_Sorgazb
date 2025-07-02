15548731fd19209c8656798d7680ab9a6153d65c
sorgazb <sorgazb@gmail.com>
2025-07-02 21:44:39 +0200

    Comienza a implementar el sistema de traduccion

---
+++ src/i18n/ui.ts
@@ -0,0 +1,42 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+};
+
+export const defaultLang = 'es';
+export const showDefaultLang = false;
+
+export const ui = {
+    es : {
+        'experience' : 'Experiencia',
+        'studies' : 'Estudios',
+        'projects' : 'Proyectos',
+        'skills' : 'Skills',
+        'about-me' : 'Sobre mí',
+        'contact' : 'Contacto',
+        'hey-soy' : 'Hey, soy',
+        'disponible-trabajar' : 'Disponible para trabajar',
+        'desarrollador' : 'Desarrollador de Aplicaciones Webs, apasionado de',
+        'aplicaciones-unicas' : 'desarrollar aplicaciones unicas.',
+        'cosas-nuevas' : 'aprender cosas nuevas.',
+        'resolver-problemas' : 'resolver problemas.',
+        'trabajar-equipo' : 'trabajar en equipo.',
+        
+    },
+    en : {
+        'experience' : 'Experience',
+        'studies' : 'Studies',
+        'projects' : 'Projects',
+        'skills' : 'Skills',
+        'about-me' : 'About me',
+        'contact' : 'Contact',
+        'hey-soy' : "Hey, I'm",
+        'disponible-trabajar' : 'Open to work',
+        'desarrollador' : 'Web Application Developer, passionate about',
+        'aplicaciones-unicas' : 'develop unique applications.',
+        'cosas-nuevas' : 'learn new things.',
+        'resolver-problemas' : 'solve problems.',
+        'trabajar-equipo' : 'work as a team.',
+    }
+} as const
+
